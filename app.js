e4657a23aed5fa29d39f66d4b2bcc0cfcea8907f
Jannik <8103530+wh1zk1d@users.noreply.github.com>
2022-12-28 19:34:58 +0100

    finishing touches

---
+++ app.js
@@ -0,0 +1,18 @@
+const allRecordButtons = document.querySelectorAll(".record button")
+
+function handleButtonClick(event) {
+  const el = event.target
+  const isSaved = el.getAttribute("data-saved")
+
+  if (isSaved) {
+    el.removeAttribute("data-saved")
+    el.innerText = "Merken"
+  } else {
+    el.setAttribute("data-saved", true)
+    el.innerText = "Gemerkt ✔️"
+  }
+}
+
+allRecordButtons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick)
+})
